4e2be8eb0a6375c627708fa130b83d80fd6c81ca
lalonsoz69 <82163318+lalonsoz69@users.noreply.github.com>
2026-02-11 14:34:03 +0100

    Update progress.js

--- js/progress.js
+++ js/progress.js
@@ -1,1 +1,13 @@
+function showProgress() {
+  const userLevel = db.get('level') || 1;
+  const maxLevel = 5;
+
+  return `
+    <h2>Tu progreso</h2>
+    <div class="progress-bar-container">
+      <div class="progress-bar" style="width:${(userLevel/maxLevel)*100}%"></div>
+    </div>
+    <p>Nivel ${userLevel} de ${maxLevel}</p>
+  `;
+}
 
