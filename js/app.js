b4b33445207edc170fd6024e9ba8152a19bee9e1
lalonsoz69 <82163318+lalonsoz69@users.noreply.github.com>
2026-02-11 13:38:18 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,7 +1,15 @@
 const app = document.getElementById("app");
 
 const routes = {
-  home: () => `<h2>Bienvenido a AccessQuest</h2><p>Aprende accesibilidad jugando.</p>`,
+  home: () => `
+  <h2>Aprende accesibilidad web jugando</h2>
+  <p>
+    AccesibilidadTest es una plataforma educativa interactiva donde aprendes
+    las normas de accesibilidad web mediante retos reales y prácticos.
+  </p>
+  <button onclick="loadRoute('game')">Empezar ahora</button>
+`
+,
   game: () => startGame(),
   progress: () => `<h2>Tu progreso</h2>`,
   teacher: () => `<h2>Panel profesor</h2>`
