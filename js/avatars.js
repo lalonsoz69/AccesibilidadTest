3ccbd052e2420d2d914ad665b46b45eaff7df66c
lalonsoz69 <82163318+lalonsoz69@users.noreply.github.com>
2026-02-12 10:12:58 +0100

    Update avatars.js

--- js/avatars.js
+++ js/avatars.js
@@ -1,18 +1,2 @@
-const avatars = ["🧑","👩","🧑‍🦱","👨‍🦰","👩‍🦳"];
-let selectedAvatar = db.get('avatar') || avatars[0];
 
-function chooseAvatar() {
-  let html = `<h2>Elige tu avatar</h2>`;
-  avatars.forEach(a => {
-    html += `<button onclick="setAvatar('${a}')" style="font-size:2rem;margin:.5rem;">${a}</button>`;
-  });
-  app.innerHTML = html;
-}
-
-function setAvatar(a) {
-  selectedAvatar = a;
-  db.set('avatar', a);
-  alert(`Avatar cambiado a ${a}`);
-  loadRoute('home');
-}
 
